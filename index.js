793b93f12f671b13d193dbdf55a3be67ded45891
Mike De Groot <mdegroot09@gmail.com>
2019-04-29 16:28:51 -0600

    added additional styling, event listener to menu button

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hi')
+let hiddenMenus = document.getElementsByClassName('hiddenMenu')
+let hiddenMenu = hiddenMenus[0]
+let menuBtns = document.getElementsByClassName('menuBtn')
+let menuBtn = menuBtns[0]
+
+function showMenu(hiddenMenu){
+  console.log('Hit')
+  if (hiddenMenu.classList.contains('expand')){
+    hiddenMenu.classList.remove('expand')
+  } else {
+    hiddenMenu.classList.add('expand')
+  }
+}
+
+menuBtn.addEventListener('click', () => showMenu(hiddenMenu))
